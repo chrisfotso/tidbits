b2ddea5d5fc4c124b6ed055f684ab32cdf366932
Chris Fotso <chrisfotso@gmail.com>
2019-05-21 16:38:55 -0500

    Created server.js to start server and connect to mongo database

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const DB_URL = `mongodb+srv://chrisadmin:Adminkflowslime17@cluster0-mealh.mongodb.net/test?retryWrites=true`;
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+const initServer = () => {
+  return app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+  });
+};
+
+const initDB = () => {
+  const options = {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  };
+
+  return mongoose
+    .connect(DB_URL, options)
+    .then(() => console.log("Connected to database"))
+    .catch(e => console.error(e));
+};
+
+initServer();
+initDB();
