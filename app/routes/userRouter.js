e057028f4ebca739defb2b3607b019d84914690a
Chris Fotso <chrisfotso@gmail.com>
2019-05-23 10:02:48 -0500

    Removed try catch blocks, trying to figure out correct error handling

--- app/routes/userRouter.js
+++ app/routes/userRouter.js
@@ -15,41 +15,31 @@ const { usernameExistsInDatabase } = require("../helpers/userFunctions");
 //Endpoint for registering a new user
 router.post("/register", verifyRegisterInput, async (req, res) => {
   const { username, password } = req.body;
-  // try {
+
   const hashedPassword = await User.hashPassword(password);
   const savedUser = await User.create({ username, password: hashedPassword });
 
   return res.status(201).send(savedUser);
-  // } catch (error) {
-  return res.status(500).send({ msg: "Unknown error occured", error });
-  // }
 });
 
 //Endpoint for logging in an already registered user
 router.post("/login", verifyLoginInput, async (req, res) => {
   const { username: bodyUsername, password } = req.body;
 
-  try {
-    //Finding user with username from body (case-insensitive)
-    const foundUser = await User.findOne({
-      username: { $regex: new RegExp(bodyUsername, "i") }
-    }).exec();
+  const foundUser = await User.findOne({
+    username: { $regex: new RegExp(bodyUsername, "i") }
+  }).exec();
 
-    //Validate password method is defined in User model file
-    const passwordsMatch = await foundUser.validatePassword(password);
+  const passwordsMatch = await foundUser.validatePassword(password);
 
-    if (!passwordsMatch) {
-      return res.status(400).send({ err: "Password is incorrect" });
-    }
-
-    //Destructuring foundUser, renaming username as databaseUsername for clarity
-    const { username: databaseUsername } = foundUser;
-    const token = jwt.sign(databaseUsername, JWT_SECRET);
-
-    return res.status(200).send({ token });
-  } catch (error) {
-    return res.status(500).send({ error });
+  if (!passwordsMatch) {
+    return res.status(400).send({ err: "Password is incorrect" });
   }
+
+  const { username: databaseUsername } = foundUser;
+  const token = jwt.sign(databaseUsername, JWT_SECRET);
+
+  return res.status(200).send({ token });
 });
 
 module.exports = router;
