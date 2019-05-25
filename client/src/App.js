df41dc62a611f792acd8de8bf700b0a5a224c6a1
Chris Fotso <chrisfotso@gmail.com>
2019-05-25 17:56:17 -0500

    Created basic register form in react to gain familiarity

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  handleUsernameChange = e => {
+    this.setState({
+      username: e.target.value
+    });
+  };
+
+  handlePasswordChange = e => {
+    this.setState({
+      password: e.target.value
+    });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    const fetchOptions = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    }
+    
+    const response = await fetch("/user/register", fetchOptions)
+    const registeredUser = await response.json()
+
+    console.log(registeredUser)
+  };
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <br />
+        <input
+          type="text"
+          name="username"
+          id="0"
+          onChange={this.handleUsernameChange}
+        />
+        <br />
+        <label htmlFor="password">Password</label>
+        <br />
+        <input
+          type="password"
+          name="password"
+          id="0"
+          onChange={this.handlePasswordChange}
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
