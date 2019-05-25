9c6d473f951fe4f0838a21b16e60e26206f81782
Chris Fotso <chrisfotso@gmail.com>
2019-05-25 17:04:44 -0500

    Created client folder

--- app/routes/tweetRouter.js
+++ app/routes/tweetRouter.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require("../models/User");
 const Tweet = require("../models/Tweet");
 
 const { verifyToken } = require("../middlewares/jwtMiddleware");
